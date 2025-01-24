cefff03ad828d8fd1f585397fc21b00ab0a5012c
Barkly-Brandon <bubba05barrera@gmail.com>
2025-01-23 21:55:06 -0700

    Add project files.

---
+++ SimpleCalc_BrandonB/SimpleCalc_BrandonB.vb
@@ -0,0 +1,21 @@
+﻿
+Module SimpleCalc_BrandonB
+
+    Sub Main()
+        Dim Value1 As Int16
+        Dim Value2 As Int16
+        Dim Operation As String
+
+        Value1 = Console.ReadLine()
+        Value2 = Console.ReadLine()
+        Operation = Console.ReadLine()
+
+        If (Operation = "Add") Or (Operation = "+") Or (Operation = "add") Or (Operation = "Plus") Or (Operation = "plus") Then
+            Console.WriteLine(Value1 + Value2)
+        ElseIf (Operation = "Subtract") Or (Operation = "-") Or (Operation = "subtract") Or (Operation = "Minus") Or (Operation = "minus") Then
+            Console.WriteLine(Value1 - Value2)
+        End If
+
+    End Sub
+
+End Module
