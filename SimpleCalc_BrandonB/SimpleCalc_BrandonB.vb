20477eab27d6ad8e355d0470e8d3bccf38c7480a
Barkly-Brandon <bubba05barrera@gmail.com>
2025-01-24 10:13:40 -0700

    Got rid of bounus features, and added user dialog

--- SimpleCalc_BrandonB/SimpleCalc_BrandonB.vb
+++ SimpleCalc_BrandonB/SimpleCalc_BrandonB.vb
@@ -1,19 +1,46 @@
-﻿
+﻿Option Explicit On
+Option Strict On
+
+'Brandon Barrera
+'Spring 2025
+'RCET2235
+'Simple Calculator
+'
+
+Imports System.Configuration
+
 Module SimpleCalc_BrandonB
 
     Sub Main()
-        Dim Value1 As Int16
-        Dim Value2 As Int16
+        Dim Value1 As Integer
+        Dim Value2 As Integer
         Dim Operation As String
 
-        Value1 = Console.ReadLine()
-        Value2 = Console.ReadLine()
+        Console.WriteLine("Enter a number")
+        Value1 = CInt(Console.ReadLine()) 'Convert user input into an integer
+        Console.WriteLine("You chose: " & Value1)
+        Console.WriteLine()
+
+        Console.WriteLine("Enter another number")
+        Value2 = CInt(Console.ReadLine())
+        Console.WriteLine("You chose: " & Value2)
+        Console.WriteLine()
+
+        Console.WriteLine("Choose a operation to perform" _
+                          & vbNewLine & "1. Add" _
+                          & vbNewLine & "2. Subtract")
         Operation = Console.ReadLine()
 
-        If (Operation = "Add") Or (Operation = "+") Or (Operation = "add") Or (Operation = "Plus") Or (Operation = "plus") Then
-            Console.WriteLine(Value1 + Value2)
-        ElseIf (Operation = "Subtract") Or (Operation = "-") Or (Operation = "subtract") Or (Operation = "Minus") Or (Operation = "minus") Then
-            Console.WriteLine(Value1 - Value2)
+        If Operation = "1" Then
+            Console.WriteLine("You chose: Additon")
+            Console.WriteLine()
+            Console.WriteLine($"{Value1} + {Value2} = " & Value1 + Value2)
+        ElseIf Operation = "2" Then
+            Console.WriteLine("You chose: Subtraction")
+            Console.WriteLine()
+            Console.WriteLine($"{Value1} - {Value2} = " & Value1 - Value2)
+        Else Console.WriteLine("Wrong! Try again.")
+
         End If
 
     End Sub
